e05a15942c4d11211bfe6de6e1e3b3b8b6d2199a
Viralink-Goddard <avidproductsint@gmail.com>
2025-10-16 21:05:34 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
-// src/pages/index.tsx
-export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui", padding: 24 }}>
-      <h1>Viralink Pro</h1>
-      <p>Home page is rendering ✅</p>
-      <p>If you can see this, the app is mounted correctly.</p>
-    </div>
-  );
-}
+/index.tsx
+import React from 'react';
+import AppLayout from '@/components/AppLayout';
+import { AppProvider } from '@/contexts/AppContext';
 
+const index: React.FC = () => {
+  return (
+    <AppProvider>
+      <AppLayout />
+    </AppProvider>
+  );
+};
+
+export default Index;
